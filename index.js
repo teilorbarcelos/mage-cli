c8233349f210b2b0bfaf891ac098872eb52f87b5
Teilor Souza Barcelos <tsb-@live.com>
2021-10-14 18:05:32 -0300

    refatoration src/next/component

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const program = require('commander')
 const package = require('./package.json')
-const fs = require('fs');
+const Nbc = require('./src/next/component')
 
 program.version(package.version)
 
@@ -14,46 +14,7 @@ program
     if (framework == 'next') {
       if (resource == 'component') {
         if (name) {
-          name = name[0].toUpperCase() + name.slice(1)
-          const dir = `./components/${name}`
-
-          if (fs.existsSync(dir)) {
-            console.log('The resource you are trying to create already exists!')
-            return
-          }
-
-          if (!fs.existsSync('./components')) {
-            fs.mkdirSync('./components')
-          }
-
-          fs.mkdirSync(dir)
-
-          const nextComponent =
-            `import styles from './styles.module.css'\r` +
-            `import globals from '../../styles/globals.module.css'\r` +
-            `\r` +
-            `export default function ${name}(){\r` +
-            `  return (\r` +
-            `    <section className={styles.${name.toLowerCase()}} id="${name.toLowerCase()}">\r` +
-            `    \r` +
-            `    </section>\r` +
-            `  )\r` +
-            `}`
-
-          const stylesModule =
-            `.${name.toLowerCase()} {\r` +
-            `  \r` +
-            `}`
-
-
-          fs.writeFileSync(`./components/${name}/index.tsx`, nextComponent, (err) => {
-            if (err) { throw err }
-          })
-
-          fs.writeFileSync(`./components/${name}/styles.module.css`, stylesModule, (err) => {
-            if (err) { throw err }
-          })
-          console.log(`Abra Kadabra!... Resource created!`)
+          Nbc(name)
         }
       }
     }
---
+++ src/next/component/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+module.exports = function (name) {
+  name = name[0].toUpperCase() + name.slice(1)
+  const dir = `./components/${name}`
+
+  if (fs.existsSync(dir)) {
+    console.log('The resource you are trying to create already exists!')
+    return
+  }
+
+  if (!fs.existsSync('./components')) {
+    fs.mkdirSync('./components')
+  }
+
+  fs.mkdirSync(dir)
+
+  const nextComponent =
+    `import styles from './styles.module.css'\r` +
+    `import globals from '../../styles/globals.module.css'\r` +
+    `\r` +
+    `export default function ${name}(){\r` +
+    `  return (\r` +
+    `    <section className={styles.${name.toLowerCase()}} id="${name.toLowerCase()}">\r` +
+    `    \r` +
+    `    </section>\r` +
+    `  )\r` +
+    `}`
+
+  const stylesModule =
+    `.${name.toLowerCase()} {\r` +
+    `  \r` +
+    `}`
+
+
+  fs.writeFileSync(`./components/${name}/index.tsx`, nextComponent, (err) => {
+    if (err) { throw err }
+  })
+
+  fs.writeFileSync(`./components/${name}/styles.module.css`, stylesModule, (err) => {
+    if (err) { throw err }
+  })
+  console.log(`Abra Kadabra!... Resource created!`)
+}
